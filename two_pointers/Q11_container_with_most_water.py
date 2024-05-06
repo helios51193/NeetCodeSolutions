68472d71723aeb6379f80fffddf62ce9e151dfdd
helios51193 <pranjal@orglens.com>
2024-05-07 02:29:49 +0530

    add Q11

---
+++ two_pointers/Q11_container_with_most_water.py
@@ -0,0 +1,15 @@
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+
+        
+        left, right = 0, len(height)-1
+        max_area = 0
+        while left < right:
+            current_area = min(height[left],height[right]) * (right-left)
+            max_area = max(max_area, current_area)
+            if height[left] > height[right]:
+                right -=1
+            else:
+                left +=1
+        
+        return max_area
