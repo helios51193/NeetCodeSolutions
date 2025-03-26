2558745cf4852a9ebfcafdf952dd54ef42e18831
helios51193 <pranjal@orglens.com>
2025-03-26 22:32:12 +0530

    Added new solution and comments for needcode 150

--- neetcode150/Q27_remove_element.py
+++ neetcode150/Q27_remove_element.py
@@ -1,3 +1,8 @@
+# Solution 1 
+# index shows where the last valid number. Invalid values are all in the right side
+from typing import List
+
+
 class Solution:
     def removeElement(self, nums: list[int], val: int) -> int:
         index = 0
@@ -6,3 +11,12 @@ class Solution:
                 nums[index] = nums[i]
                 index +=1
         return index
+
+# Solution 2 
+# return the len of array with removed value
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        temp = [x for x in nums if x != val]
+
+        nums[:] = temp
+        return len(nums)
--- neetcode150/Q88_merge_sorted_array.py
+++ neetcode150/Q88_merge_sorted_array.py
@@ -1,3 +1,8 @@
+# Solution 1 
+# Copy all the values in the of num2 in num1 and then sort
+from typing import List
+
+
 class Solution:
     def merge(self, nums1: list[int], m: int, nums2: list[int], n: int) -> None:
         """
@@ -7,3 +12,42 @@ class Solution:
             nums1[i] = nums2[i-m]
         
         nums1.sort()
+
+# Solution 2
+# Merge the two array in a third array (like it is done in merge sort) and then copy the temp array to the nums1 
+class Solution2:
+    def merge(self, nums1: List[int], m: int, nums2: List[int], n: int) -> None:
+        """
+        Do not return anything, modify nums1 in-place instead.
+        """
+        if n < 1:
+            return
+        
+        nums3 = [0 for x in range(m+n)]
+        i = 0
+        j = 0
+        k = 0
+        while i < m  and j < n:
+            
+            if nums1[i] < nums2[j]:
+                nums3[k] = nums1[i]
+                i += 1
+            else:
+                nums3[k] = nums2[j]
+                j += 1
+            
+            k +=1
+            
+        while j < n:
+            nums3[k] = nums2[j]
+            k += 1
+            j += 1
+        
+        while i < m:
+            nums3[k] = nums1[i]
+            k += 1
+            i += 1
+        
+        for index, num in enumerate(nums3):
+            nums1[index] = num
+        
