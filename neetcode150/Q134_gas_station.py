5092a401957e1a86d8483753e4e3adcaa26161d3
helios51193 <pranjal@orglens.com>
2025-04-06 18:35:47 +0530

    added 134,135 and 238

---
+++ neetcode150/Q134_gas_station.py
@@ -0,0 +1,21 @@
+
+from typing import List
+
+
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        
+        # if total gas is less than total cose ,return
+        if sum(gas) < sum(cost):
+            return -1
+        
+        tank = idx = 0
+        # start from 0, if the tank is -ve, reset the tank idx to 0
+        # For any value a, if the tank becomes negative. There there is no solution at a or before a a
+        # as values before a will just make the tank more negative.
+        for i in range(len(gas)):
+            tank += gas[i] - cost[i]
+            if tank < 0:
+                tank, idx = 0, i+1
+
+        return idx
---
+++ neetcode150/Q135_candy.py
@@ -0,0 +1,58 @@
+from typing import List
+
+# One pass solution
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        n = len(ratings)
+        total_candies = n
+        i = 1
+
+        while i < n:
+            # if both rating are same , increment i and continue
+            if ratings[i] == ratings[i-1]:
+                i +=1
+                continue
+            
+
+            # loop till current has high rating than prev, update the peak 
+            current_peak = 0
+            while i < n and ratings[i] > ratings[i-1]:
+                current_peak +=1
+                total_candies += current_peak
+                i += 1
+            
+            # return if there are rating in ascending order
+            if i == n:
+                return total_candies
+            
+            # loop till current has lower rating than prev, update valley
+            current_valley = 0
+            while i < n and ratings [i] < ratings[i-1]:
+                current_valley +=1
+                total_candies += current_valley
+                i +=1
+            
+            # remove the minumum of peak and valley as we add  peack and valley 2 times. 
+            total_candies -= min(current_peak, current_valley)
+        
+        return total_candies
+
+# two pass solution
+# also retains the candy distribution
+class Solution:
+    def candy(self, ratings: List[int]) -> int:
+        
+        n = len(ratings)
+        candies = [1 for _ in range(n)]
+
+        for i in range(1,n):
+            if ratings[i-1] < ratings[i] and candies[i-1] >= candies[i]:
+                candies[i] = candies[i-1] + 1
+        
+        for i in range(n-2, -1, -1):
+            if ratings[i+1] < ratings[i] and candies[i+1] >= candies[i]:
+                candies[i] = candies[i+1] + 1
+        
+        total_candies = sum(candies)
+
+        return total_candies
