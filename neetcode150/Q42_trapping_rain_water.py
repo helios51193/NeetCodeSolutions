a9c9441326f71cd4aa79151ed0458801152a88c8
helios51193 <pranjal@orglens.com>
2025-04-07 21:10:15 +0530

    added commments in Q42

---
+++ neetcode150/Q42_trapping_rain_water.py
@@ -0,0 +1,22 @@
+from typing import List
+
+# This idea is to find the left and right highest walls to capture maximum water
+class Solution:
+    def trap(self, height: List[int]) -> int:
+        left,right = 0, len(height)-1
+        max_left = height[left]
+        max_right = height[right]
+        trapped_water = 0
+        while left < right:
+            # check which max is lower and process on that.
+            # find max and calculate the trapped water based on max and current height
+            if max_left < max_right:
+                left +=1
+                max_left = max(max_left, height[left])
+                trapped_water += max_left - height[left]
+            else:
+                right -=1
+                max_right = max(max_right, height[right])
+                trapped_water += max_right - height[right]
+
+        return trapped_water       
