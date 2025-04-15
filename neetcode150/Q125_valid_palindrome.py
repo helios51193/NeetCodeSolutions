5f42c0578198992c78cdc207c69bf342864a3ce4
helios51193 <pranjal51193@gmail.com>
2025-04-15 21:05:06 +0530

    added two pointer problems

---
+++ neetcode150/Q125_valid_palindrome.py
@@ -0,0 +1,11 @@
+import re
+
+
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        cleaned = re.sub(r"[^a-zA-Z0-9]", "", s).lower()
+        
+        if cleaned == "" or cleaned == " ":
+            return True
+        
+        return cleaned == cleaned[::-1]
---
+++ neetcode150/Q167_two_sum_2.py
@@ -0,0 +1,16 @@
+from typing import List
+
+
+class Solution:
+    def twoSum(self, numbers: List[int], target: int) -> List[int]:
+        left, right = 0, len(numbers)-1
+        while(left < right):
+            numsum = numbers[left] + numbers[right]
+            if numsum == target:
+                return [left+1, right+1]
+            if numsum > target:
+                right -=1
+            else:
+                left += 1
+        
+        return []
