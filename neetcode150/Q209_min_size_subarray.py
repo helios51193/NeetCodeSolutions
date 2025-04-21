68e636854f568f73236e27c9326841a2408717ec
helios51193 <pranjal@orglens.com>
2025-04-21 20:42:30 +0530

    added some more sliding window questions

---
+++ neetcode150/Q209_min_size_subarray.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def minSubArrayLen(self, target: int, nums: List[int]) -> int:
+        start = curr = 0
+        n = len(nums)
+        minSize = n + 1
+        for i in range(n):
+            
+            curr += nums[i] #expand the window
+            #print(f"Expanding : {curr}")
+            while curr >= target:
+                minSize = min(minSize, i-start + 1) # update minsize
+                curr -= nums[start] #shrink the window
+                #print(f"Shrinking : {curr}  {i-start+1}")
+                start += 1 
+                
+        
+        return 0 if minSize > n else minSize
