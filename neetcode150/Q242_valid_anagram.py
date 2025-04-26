57ab0cce356d418fabca8aecc30f538402b4ba85
helios51193 <pranjal@orglens.com>
2025-04-26 20:58:35 +0530

    anagrams

---
+++ neetcode150/Q242_valid_anagram.py
@@ -0,0 +1,29 @@
+# using hashmap
+from collections import Counter
+
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        
+        if len(s) != len(t):
+            return False
+        
+        char_map = {}
+        for char in s:
+            char_map[char] = char_map.get(char,0) + 1
+        
+        for char in t:
+            if char not in char_map:
+                return False
+            
+            char_map[char] -= 1
+            if char_map[char] < 0:
+                return False
+        
+        return True
+
+
+# using Counter
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
