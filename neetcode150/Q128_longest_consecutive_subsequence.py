a3a9bd3f412c7e6716acbaeaff8f48d018339054
helios51193 <pranjal51193@gmail.com>
2025-04-29 21:22:12 +0530

    Added interval questions

---
+++ neetcode150/Q128_longest_consecutive_subsequence.py
@@ -0,0 +1,22 @@
+from typing import List
+
+
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+
+        if len(nums) == 0:
+            return 0
+        if len(nums) == 1:
+            return 1
+        
+
+        nums_set = set(nums)
+        mx = 0
+        for num in nums:
+            if num-1 not in nums_set:
+                tmp = 1
+                while num+tmp in nums_set:
+                    tmp +=1
+                mx = max(mx,tmp)
+        
+        return mx
