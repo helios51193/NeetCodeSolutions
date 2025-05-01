c18b120a8988d10c8a24cdb64618b5df76a1c538
helios51193 <pranjal51193@gmail.com>
2025-05-01 21:33:23 +0530

    added interval questions

---
+++ neetcode150/Q452_min_no_of _arrows_to_burst_balloon.py
@@ -0,0 +1,18 @@
+# It is same as grouping the intervals , the difference 
+# You sort it first, and group them till ending of current is greater then start of current point.
+# One group can popped by 1 arrow.
+from typing import List
+class Solution:
+    def findMinArrowShots(self, points: List[List[int]]) -> int:
+        
+        # sorting by end coordinates
+        points.sort(key = lambda x:x[1])
+        print(points)
+        pop = points[0][1] # pop point
+        arrow = 1 # number of arrows initialized
+        for start,end in points:
+            if pop < start:
+                arrow += 1
+                pop = end
+        
+        return arrow
