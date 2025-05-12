eb08d167567c90654f3b5031a5449ff35d6c5f4f
helios51193 <pranjal@orglens.com>
2025-05-12 22:25:08 +0530

    Added more linked list questions

---
+++ neetcode150/Q138_copy_list_with_random_pointer.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+"""
+from typing import Optional
+
+
+class Node:
+    def __init__(self, x: int, next: 'Node' = None, random: 'Node' = None):
+        self.val = int(x)
+        self.next = next
+        self.random = random
+
+class Solution:
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        
+        if not head:
+            return None
+
+        # Create a hashmap with node -> new node with val
+        old_new_map = {}
+        
+        curr = head
+        while curr:
+            old_new_map[curr] = Node(curr.val)
+            curr = curr.next
+        
+        # Populate the next and random of new nodes based on map
+        curr = head
+        while curr:
+            old_new_map[curr].next = old_new_map.get(curr.next)
+            old_new_map[curr].random = old_new_map.get(curr.random)
+            curr = curr.next
+        
+        return old_new_map[head]
