1568bfdef68b017be5d05991e40e4244abf8a016
helios51193 <pranjal51193@gmail.com>
2025-06-02 20:19:00 +0530

    Link List questions

---
+++ neetcode150/Q61_rotate_list.py
@@ -0,0 +1,33 @@
+# Definition for singly-linked list.
+from typing import Optional
+
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+class Solution:
+    def rotateRight(self, head: Optional[ListNode], k: int) -> Optional[ListNode]:
+        
+        if not head:
+            return head
+        
+        # connect tail to head
+        curr = head
+        length = 1
+        while curr.next:
+            curr =curr.next
+            length += 1
+        
+        curr.next = head
+
+        # move to new head
+        k = length - (k%length)
+        while k > 0:
+            curr = curr.next
+            k -=1
+        
+        # disconnect and return new head
+        newhead = curr.next
+        curr.next = None
+        return newhead
